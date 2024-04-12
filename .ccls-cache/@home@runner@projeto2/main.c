3bee71ee26c05145dc1711db72d6926ee56520c4
Matheus Eleutério <100217154+matheus-eleuterio@users.noreply.github.com>
2024-04-12 16:08:12 +0000

    remoção texto extra

--- .ccls-cache/@home@runner@projeto2/main.c
+++ .ccls-cache/@home@runner@projeto2/main.c
@@ -5,11 +5,11 @@ int main(){
     funcao fs[] = {criar, deletar, listar, salvar, carregar};
 
     Tarefa tarefas[TOTAL];
-    int pos = 0; // Inicializa a posição com 0
-    ERROS erro = fs[4](tarefas, &pos); // Carrega as tarefas do arquivo
+    int pos = 0; 
+    ERROS erro = fs[4](tarefas, &pos); 
     if (erro != OK) {
         printf("Erro ao carregar as tarefas. Código de erro: %d\n", erro);
-        return 1; // Retorna um código de erro para indicar falha
+        return 1; 
     }
 
     int opcao;
@@ -24,24 +24,24 @@ int main(){
         scanf("%d", &opcao);
         if (opcao == 0) {
             printf("Você saiu do programa\n");
-            break; // Sai do loop ao digitar 0
+            break; 
         }
 
         if (opcao < 1 || opcao > 3) {
             printf("Opcao invalida\n");
         } else {
-            erro = fs[opcao - 1](tarefas, &pos); // Chama a função correspondente à opção
+            erro = fs[opcao - 1](tarefas, &pos); 
             if (erro != OK) {
                 printf("Erro ao executar a operacao. Código de erro: %d\n", erro);
             }
         }
     } while (1);
 
-    erro = fs[3](tarefas, &pos); // Salva as tarefas no arquivo
+    erro = fs[3](tarefas, &pos); 
     if (erro != OK) {
         printf("Erro ao salvar as tarefas. Código de erro: %d\n", erro);
-        return 1; // Retorna um código de erro para indicar falha
+        return 1; 
     }
 
-    return 0; // Retorna 0 para indicar sucesso
+    return 0; 
 }
